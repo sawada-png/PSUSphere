85e5025de82337fe1c7cc9eb7a2b0d246281ccbe
Nathan Brix Grande <brixLG1806@gmail.com>
2026-09-02 09:43:25 +0800

    org

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -7,3 +7,10 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5
