c9546718f90c04cb9632ae80ae2e38afc5c4f608
Nathan Brix Grande <brixLG1806@gmail.com>
2026-09-02 20:19:22 +0800

    05 Search, Login, & Logout

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,6 @@
+from django.db.models import Q
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -6,10 +9,34 @@ from .models import Organization, College, Program, Student, OrgMember
 from .forms import OrganizationForm, CollegeForm, ProgramForm, StudentForm, OrgMemberForm
 
 
-class HomePageView(ListView):
+class HomePageView(LoginRequiredMixin, ListView):
     model = Organization
     context_object_name = "home"
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["organization_count"] = Organization.objects.count()
+        context["college_count"] = College.objects.count()
+        context["program_count"] = Program.objects.count()
+        context["student_count"] = Student.objects.count()
+
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+
+        return context
 
 
 class OrganizationList(ListView):
@@ -17,6 +44,18 @@ class OrganizationList(ListView):
     context_object_name = "organization"
     template_name = "org_list.html"
     paginate_by = 5
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        if query:
+            qs = qs.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+
+        return qs
 
 
 class OrganizationCreateView(CreateView):
@@ -66,7 +105,18 @@ class CollegeDeleteView(DeleteView):
 
 class ProgramListView(ListView):
     model = Program
+    context_object_name = "program"
     template_name = "program_list.html"
+    paginate_by = 5
+
+    def get_ordering(self):
+        allowed = ["prog_name", "college__college_name"]
+        sort_by = self.request.GET.get("sort_by")
+
+        if sort_by in allowed:
+            return sort_by
+
+        return "prog_name"
 
 
 class ProgramCreateView(CreateView):
